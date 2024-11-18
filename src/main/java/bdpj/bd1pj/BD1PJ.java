e6116e8a847c1e3e5cb55345947f55ce7fb898dd
Benjamin Urbina <benjamin.urbina@userena.cl>
2024-11-18 20:13:29 -0300

    Subiendo bases

---
+++ src/main/java/bdpj/bd1pj/BD1PJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bdpj.bd1pj;
+
+/**
+ *
+ * @author benjaminurbinarusque
+ */
+public class BD1PJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
